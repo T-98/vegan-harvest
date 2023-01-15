b3ea2255a8cb79a71dc42a0115644e7417e289ab
Jan <janing@ucsc.edu>
2023-01-15 01:12:11 -0800

    Added settings menu

--- UMPGorePack/Intermediate/Build/Win64/UMPGorePackEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++ UMPGorePack/Intermediate/Build/Win64/UMPGorePackEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30146
-#include "D:/Vegan Zombies/vegan-harvest/UMPGorePack/Intermediate/Build/Win64/UMPGorePackEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
+// Compiler: 14.29.30138
+#include "E:/GitHub/Harvest/vegan-harvest/UMPGorePack/Intermediate/Build/Win64/UMPGorePackEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
